a6cca0dd7651378012b046fe79062d2ee9faec1f
enzosantosnascimento-png <enzo.santos.nascimento@escola.pr.gov.br>
2026-03-26 08:02:41 -0300

    Add files via upload

---
+++ Aula03/src/Aluno.java
@@ -0,0 +1,40 @@
+public class Aluno {
+    private String nome;
+    private Integer idade;
+    private String curso;
+    private String turma;
+    
+    public Aluno() {
+    }
+    public Aluno(String nome, Integer idade, String curso, String turma) {
+        this.nome = nome;
+        this.idade = idade;
+        this.curso = curso;
+        this.turma = turma;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+    public String getCursos() {
+        return curso;
+    }
+    public void setCursos(String curso) {
+        this.curso = curso;
+    }
+    public String getTurma() {
+        return turma;
+    }
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+}
